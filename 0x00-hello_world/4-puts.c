551a74792580ded858af8f4d614b6c50b26c58b7
root <mohamedkhdeer123@gmail.com>
2023-03-20 04:51:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -A line that prints a line with pust function
+n
+ * return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
